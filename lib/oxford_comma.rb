b252bf30286979fc622e7a1edb2529f700a7c109
Jason Keeling <s2jason@yahoo.com>
2020-03-20 01:39:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,9 @@ def oxford_comma(array)
     array[-2] << " and "
     array.join
   elsif array.length == 1
-    array[-1] << " "
-    array.join
+      array.join
   elsif array.length > 2
-    array[-1].prepend "and"
+    array[-1].prepend "and "
     array.join(",")
   end
 end
